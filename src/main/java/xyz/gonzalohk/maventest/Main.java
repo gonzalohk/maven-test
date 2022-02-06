feb2629f7611e470e018c53aabaeba48f05a059e
Gonzalo Osco <goh.acm@gmail.com>
2022-02-06 15:00:05 -0400

    TEST and PROD profiles were created

--- src/main/java/xyz/gonzalohk/maventest/Main.java
+++ src/main/java/xyz/gonzalohk/maventest/Main.java
@@ -1,6 +1,5 @@
 package xyz.gonzalohk.maventest;
-import java.io.File;
-import java.io.FileInputStream;
+
 import java.util.Properties;
 
 import static spark.Spark.*;
@@ -13,7 +12,8 @@ public class Main {
 
     public void mappingRoutes(Properties properties) {
         get(TEST_PATH, (req, res) -> properties.getProperty("TEST_MESSAGE"));
-        get(ENTORNOS_PATH, (req, res) -> String.format(properties.getProperty("ENTORNO_MESSAGE"), "PROD"));
+        get(ENTORNOS_PATH, (req, res) -> String.format(properties.getProperty("ENTORNO_MESSAGE"),
+                properties.getProperty("ENTORNO_LABEL")));
     }
 
     public void startServer(Properties properties) {
@@ -23,8 +23,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Properties properties = new Properties();
-        properties.load(new FileInputStream(new File("src/main/resources/vars.properties")));
-
+        properties.load(Main.class.getClassLoader().getResourceAsStream("vars.properties"));
         Main main =new Main();
         main.startServer(properties);
     }
